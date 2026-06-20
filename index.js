733e344123332e51a15fc8f9385278513f7ce6ab
Gustavo Morais <gusmorais.18@gmail.com>
2026-06-20 18:41:04 -0300

    fundamentos de teste

---
+++ index.js
@@ -0,0 +1,13 @@
+let meta = {
+    value: 'ler um livro por mês',
+    checked: false,
+    log: (info) => {
+        console.log(info)
+    }
+}
+
+meta.value = "não é mais ler um livro"
+meta.log(meta.value) 
+
+
+
